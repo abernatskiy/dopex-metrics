00341d38594cfae021ca6a494d51557fc903a548
abernatskiy <abernatskiy@toha.li>
2023-01-19 21:17:41 +0900

    A working rDPX supply tracker
    	modified:   src/processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -1,42 +1,53 @@
 import { TypeormDatabase } from "@subsquid/typeorm-store";
-import {EvmBatchProcessor} from '@subsquid/evm-processor'
+import { EvmBatchProcessor } from '@subsquid/evm-processor'
+import * as rdpxabi from './abi/arb-rdpx-token'
+import * as proxyabi from './abi/proxy'
 
 const processor = new EvmBatchProcessor()
-  .setDataSource({
-    // uncomment and set RPC_ENDPOONT to enable contract state queries. 
-    // Both https and wss endpoints are supported. 
-    // chain: process.env.RPC_ENDPOINT,
+	.setBlockRange({from: 1950162}) // the deployment transaction of 0x32eb7902d4134bf98a28b963d26de779af92a212
+	.setDataSource({
+		// uncomment and set RPC_ENDPOONT to enable contract state queries.
+		// Both https and wss endpoints are supported.
+		// chain: process.env.RPC_ENDPOINT,
+		archive: 'https://arbitrum.archive.subsquid.io',
+	})
+	.addLog('0x32eb7902d4134bf98a28b963d26de779af92a212', {
+		filter: [[rdpxabi.events.Transfer.topic]],
+		data: {
+			evmLog: { id: true, topics: true, data: true },
+			transaction: { hash: true, from: true, to: true, input: true }
+		} as const
+	})
 
-    // Change the Archive endpoints for run the squid 
-    // against the other EVM networks
-    // For a full list of supported networks and config options
-    // see https://docs.subsquid.io/develop-a-squid/evm-processor/configuration/
+function printAtBlock(c: any, msg: string) {
+	console.log(`${c.header.height}: ${msg}`)
+}
 
-    archive: 'https://eth.archive.subsquid.io',
-  })
-  .addTransaction([
-    '0x0000000000000000000000000000000000000000'
-  ], {
-    range: {
-      from: 6_000_000
-    },
-    data: {
-      transaction: {
-        from: true,
-        input: true,
-        to: true
-      }
-    }
-  });
-
+var totalCoinz: bigint = 0n
 
 processor.run(new TypeormDatabase(), async (ctx) => {
-  for (let c of ctx.blocks) {
-    for (let i of c.items) {
-      // apply arbitrary data transformation logic here
-      // use ctx.store to persist the data
-      ctx.log.info(i, "Next item:")
-    }
-  }
-});
+	for (let c of ctx.blocks) {
+		for (let i of c.items) {
+			if(i.kind==='evmLog') {
+				if(i.address!=='0x32eb7902d4134bf98a28b963d26de779af92a212') {
+					printAtBlock(c, `got log with an unexpected address`)
+					console.log(i)
+				}	else {
+					let {from, to, value} = rdpxabi.events.Transfer.decode(i.evmLog)
+					if(from==='0x0000000000000000000000000000000000000000') {
+						totalCoinz += value.toBigInt()
+						printAtBlock(c, `${value} was minted at ${to} as a result of txn:\n${i.transaction.from} -> ${i.transaction.to}\n${i.transaction.input}\nRunning supply ${totalCoinz}\n`)
+					}
+					else if(to==='0x0000000000000000000000000000000000000000') {
+						totalCoinz -= value.toBigInt()
+						printAtBlock(c, `${value} was burned at ${from} as a result of txn:\n${i.transaction.from} -> ${i.transaction.to}\n${i.transaction.input}\nRunning supply ${totalCoinz}\n`)
+					}
+				}
+			}
+			else {
+//				console.log(i)
+			}
+		}
+	}
 
+});
